a2c7c09ce70f0c164e05cac3b38e6f590edbca80
frank07080 <frank.guanzhongchen@gmail.com>
2024-03-21 01:51:49 +0800

    bug fix for humble

--- launch/10-head.launch.py
+++ launch/10-head.launch.py
@@ -33,10 +33,10 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
-from launch_ros.substitutions import FindPackageShare, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
